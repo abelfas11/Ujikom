81a830c453466f89d3657c5bdf334fb1dc8ed783
abelfas <abelfas12@gmail.com>
2025-02-05 13:33:14 +0700

    add databse to login

--- database/migrations/2014_10_12_000000_create_siswa_table.php
+++ database/migrations/2014_10_12_000000_create_siswa_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('siswa', function (Blueprint $table) {
             $table->string('nama_lengkap');
             $table->string('username');
             $table->string('password');
